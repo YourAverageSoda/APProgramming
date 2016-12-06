00ac33d222ecd0f213ed496c584de99efc77430d
YourAverageRandy <andy.zheng.v3@gmail.com>
2016-12-06 13:58:09 -0600

    Initialize

--- Program0/src/program0/Program0.java
+++ Program0/src/program0/Program0.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package program0;
-
-/**
- *
- * @author andy_zheng422
- */
+/*  Author: Andy Zheng
+    Date Created: 12/6/16
+*/
 public class Program0 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
 }
