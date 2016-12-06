c7591dd1777b4a7980679df0ebd3c8cacbd0e22a
YourAverageRandy <andy.zheng.v3@gmail.com>
2016-12-06 14:19:24 -0600

    helloWorld

---
+++ Program0/src/Unit01/helloWorld.java
@@ -0,0 +1,25 @@
+package Unit01;
+
+/*  Author: Andy Zheng
+    Date Created: 12/06/16
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+        System.out.println("123");
+        System.out.println("My name is \"Me\".");
+        for(int i = 0; i < 3; i++) {
+            System.out.println("#####\t\t\t#####");
+        }
+        for(int i = 0; i < 2; i++) {
+            System.out.println("");
+        }
+        System.out.println("#####\t\t\t#####");
+        for(int i = 0; i < 2; i++){
+            for(int x = 0; x < 29; x++) {
+                System.out.print("#");
+            }
+            System.out.println("");
+        }
+    }
+}
