8bf05b4ca580064acec21bd50cffc6f8e4d246dd
YourAverageRandy <andy.zheng.v3@gmail.com>
2016-12-07 13:33:29 -0600

    helloWorld Smiley

--- Program0/src/Unit01/helloWorld.java
+++ Program0/src/Unit01/helloWorld.java
@@ -7,7 +7,7 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World.");
         System.out.println("123");
-        System.out.println("My name is \"Me\".");
+        System.out.println("My name is \"Me\".\n");
         for(int i = 0; i < 3; i++) {
             System.out.println("#####\t\t\t#####");
         }
@@ -15,8 +15,13 @@ public class helloWorld {
             System.out.println("");
         }
         System.out.println("#####\t\t\t#####");
-        for(int i = 0; i < 2; i++){
-            for(int x = 0; x < 29; x++) {
+        for(int i = 0; i < 1; i++){
+            System.out.print(" #######\t     #######\n   ");
+            for(int x = 0; x < 23; x++) {
+                System.out.print("#");
+            }
+            System.out.print("\n       ");
+            for(int x = 0; x < 15; x++) {
                 System.out.print("#");
             }
             System.out.println("");
