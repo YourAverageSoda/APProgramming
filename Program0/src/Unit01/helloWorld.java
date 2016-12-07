d4e65790ba71996cd6414c06aa4d82c04f0d9c79
YourAverageRandy <andy.zheng.v3@gmail.com>
2016-12-07 14:06:38 -0600

    helloWorld String

--- Program0/src/Unit01/helloWorld.java
+++ Program0/src/Unit01/helloWorld.java
@@ -8,21 +8,22 @@ public class helloWorld {
         System.out.println("Hello World.");
         System.out.println("123");
         System.out.println("My name is \"Me\".\n");
+        String charUsed = "*";
         for(int i = 0; i < 3; i++) {
-            System.out.println("#####\t\t\t#####");
+            System.out.println(charUsed + charUsed + charUsed + charUsed + charUsed + "\t\t\t" + charUsed + charUsed + charUsed + charUsed + charUsed);
         }
         for(int i = 0; i < 2; i++) {
             System.out.println("");
         }
-        System.out.println("#####\t\t\t#####");
+        System.out.println((charUsed + charUsed + charUsed + charUsed + charUsed) + "\t\t\t" + (charUsed + charUsed + charUsed + charUsed + charUsed));
         for(int i = 0; i < 1; i++){
-            System.out.print(" #######\t     #######\n   ");
+            System.out.print(" " + (charUsed + charUsed + charUsed + charUsed + charUsed + charUsed + charUsed) + "\t     " + (charUsed + charUsed + charUsed + charUsed + charUsed + charUsed + charUsed) + "\n   ");
             for(int x = 0; x < 23; x++) {
-                System.out.print("#");
+                System.out.print(charUsed);
             }
             System.out.print("\n       ");
             for(int x = 0; x < 15; x++) {
-                System.out.print("#");
+                System.out.print(charUsed);
             }
             System.out.println("");
         }
