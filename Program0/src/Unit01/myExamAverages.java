a6eb6b43975ca85c0f366ac1a92e9a1134159c25
YourAverageRandy <andy.zheng.v3@gmail.com>
2016-12-09 13:14:32 -0600

    myExamAverages Cleaned

--- Program0/src/Unit01/myExamAverages.java
+++ Program0/src/Unit01/myExamAverages.java
@@ -10,11 +10,11 @@ public class myExamAverages {
         int ex4 = 71, ex5 = 92, ex6 = 88;
         double sum = (double)(ex1 + ex2 + ex3 + ex4 + ex5 + ex6);
         double average0 = (sum / 6.0);
-        double average1 = (int)(average0 * 10);
-        double average2 = (int)(average0 * 100);
+        double average1 = (int)(average0 * 10 + .5);
+        double average2 = (int)(average0 * 100 + .5);
         int roundedSum0 = (int)(average0 + .5);
-        double roundedSum1 = (average1 / 10.0 +.05);
-        double roundedSum2 = (average2 / 100.0 + .005);
+        double roundedSum1 = (average1 / 10.0);
+        double roundedSum2 = (average2 / 100.0);
         System.out.print("Avg rounded to nearest percent: " + roundedSum0);
         System.out.print("\nAvg rounded to one decimal place: " + roundedSum1);
         System.out.print("\nAvg rounded to two decimal places: " + roundedSum2);
