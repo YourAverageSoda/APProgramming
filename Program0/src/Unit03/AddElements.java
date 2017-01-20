aa5dd2da3e6a8990f5c46a3c4caadf45072c5554
YourAverageRandy <andy.zheng.v3@gmail.com>
2017-01-20 13:14:07 -0600

    Unit 04 Start

---
+++ Program0/src/Unit03/AddElements.java
@@ -0,0 +1,35 @@
+package Unit03;
+
+/*
+Author: Andy Zheng
+Date Created: 1/12/17
+*/
+public class AddElements {
+    public static void main(String[] args) {
+        int[] elesis = {23,11,22,7,1,3,6,8,14};
+        int[][] elsword = new int[2][3];
+        int raven = 0;
+        elsword[1][1] = 6;
+        elsword[1][2] = 7;
+        elsword[0][0] = 8;
+        for(int i = 0; i < elsword.length; i++){
+            for(int j = 0; j < elsword[0].length; j++){
+                if(elsword[i][j] == 0){
+                    elsword[i][j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < elsword.length; i++){
+            for(int j = 0; j < elsword[0].length; j++){
+                System.out.print(elsword[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+        for(int i = 0; i < elsword.length; i++){
+            for(int j = 0; j < elsword[0].length; j++){
+                raven += elsword[i][j];
+            }
+        }
+        System.out.println("Total is " + raven + ".");
+    }
+}
